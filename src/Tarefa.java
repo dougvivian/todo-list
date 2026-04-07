8e89cf973801208e69b41e0510454cc55aeb1b24
Douglas2 <doug_vivian@hotmail.com>
2026-04-07 12:01:50 -0300

    criei primeira estrutura de tarefa e main

---
+++ src/Tarefa.java
@@ -0,0 +1,77 @@
+import java.time.LocalDate;
+
+public class Tarefa {
+    private int id;
+    private String descricao;
+    private boolean concluida;
+    private LocalDate dataCadastro;
+    private LocalDate dataLimite;
+
+
+    public Tarefa(String descricao, LocalDate dataLimite){
+        this.descricao = descricao;
+        this.concluida = false;
+        this.dataCadastro = LocalDate.now();
+        this.dataLimite = dataLimite;
+
+    }
+    
+    public void concluir(){
+        setConcluida(true);
+    }
+
+    public String toString(){
+        return "Tarefa" + descricao;
+
+    }
+
+
+
+
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return this.concluida;
+    }
+
+    public boolean getConcluida() {
+        return this.concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public LocalDate getDataCadastro() {
+        return this.dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public LocalDate getDataLimite() {
+        return this.dataLimite;
+    }
+
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+}
